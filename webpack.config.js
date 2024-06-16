e40a7984faa68708068604e57bc195d676868c72
SierraMoiseevna <utv.header@gmail.com>
2024-06-16 18:14:28 +0300

    assembly configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
+    path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+  })
+],
   mode: 'development',
   devServer: {
     static: {
-      directory: path.resolve(__dirname),
+      directory: path.resolve(__dirname, 'dist'),
     },
     compress: true,
     port: 8080,
